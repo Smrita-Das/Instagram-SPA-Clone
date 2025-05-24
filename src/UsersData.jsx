15ff1961f08d024574325759b0e7fde6f259cfc4
Smrita Das <1003sdas@gmail.com>
2025-05-24 12:38:23 +0530

    modified users data

--- src/UsersData.jsx
+++ src/UsersData.jsx
@@ -1,42 +1,77 @@
-const users = [
+const  UsersData = [
   {
     name: "Aarav Mehta",
+    username: "aarav_mehta",
+    description: "Coding life, coffee, and clean commits ☕💻",
+    followers: 1240,
+    following: 530,
     designation: "Software Engineer",
     image: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: "Priya Sharma",
+    username: "priya.sharma",
+    description: "Building dreams one product at a time 🚀",
+    followers: 2380,
+    following: 720,
     designation: "Product Manager",
     image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: "Rahul Verma",
+    username: "rahul.designs",
+    description: "Designing with purpose 🎨 | UX matters",
+    followers: 1875,
+    following: 600,
     designation: "UI/UX Designer",
     image: "https://plus.unsplash.com/premium_photo-1682096252599-e8536cd97d2b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: "Ananya Iyer",
+    username: "ananya_ai",
+    description: "Turning data into stories 📊 | AI believer",
+    followers: 3010,
+    following: 980,
     designation: "Data Scientist",
     image: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: "Karan Patel",
+    username: "karan.codes",
+    description: "API wizard 🧙‍♂️ | Backend is the backbone",
+    followers: 950,
+    following: 410,
     designation: "Backend Developer",
     image: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: "Sneha Reddy",
+    username: "sneha.markets",
+    description: "Marketing magic ✨ | Storytelling through strategy",
+    followers: 2080,
+    following: 870,
     designation: "Marketing Executive",
     image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
   },
   {
     name: "Vikram Singh",
+    username: "vikram_devops",
+    description: "Cloud. CI/CD. Containers. That’s the vibe ☁️🐳",
+    followers: 1785,
+    following: 630,
     designation: "DevOps Engineer",
     image: "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBvcnRyYWl0fGVufDB8fDB8fHww"
   },
   {
     name: "Neha Kapoor",
+    username: "neha.qa",
+    description: "Breaking code to make it stronger 🔍",
+    followers: 1220,
+    following: 540,
     designation: "Quality Analyst",
     image: "https://images.unsplash.com/photo-1580489944761-15a19d654956?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBvcnRyYWl0fGVufDB8fDB8fHww"
   }
 ];
+
+
+export default UsersData;
